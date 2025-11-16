077e7fb7028c331460e24b9155406eea95736c4a
SandeepGoswami18 <sandeepgoswami979397@gmail.in>
2025-11-16 16:14:05 +0530

    update pattern

--- pattern1.java
+++ pattern1.java
@@ -142,11 +142,30 @@ public class pattern1 {
             System.out.println();
         }
     }
+    void print12(int n){
+        int space=2*(n-1);
+        for(int i=1;i<=n;i++){
+            //number
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            //space
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            //number
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            space-=2;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         pattern1 obj = new pattern1();
-        obj.print11(n);
+        obj.print12(n);
         
     }
 }
